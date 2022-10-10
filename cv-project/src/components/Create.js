fa54c64c058a7f083d1dab27c727dc9f4d82bbd3
Taaaaab <owen.whitsett@gmail.com>
2022-10-10 19:58:10 -0400

    Change to class component using setState

--- cv-project/src/components/Create.js
+++ cv-project/src/components/Create.js
@@ -1,49 +1,113 @@
-import { useState } from "react";
+import React, { Component } from "react";
+import Object from "./Object";
+import uniqid from "uniqid";
 import "../App.css";
 
-const Create = () => {
-    const [fullName, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const inputObj = { fullName, email, phone}
-        console.log(inputObj);
+class Create extends Component {
+    constructor() {
+      super();
+  
+      this.state = {
+        fullName: '', 
+        email: '', 
+        phone: '',
+        id: uniqid(),
+        infoArray: [],
+      };
     }
+   
+    handleNameChange = (e) => {
+      this.setState({
+        fullName
+      });
+    };
+  
+    onSubmitTask = (e) => {
+      e.preventDefault();
+      console.log(this.in);
+      this.setState({
+          infoArray: this.state.infoArray.concat(this.state.info),
+          info: { 
+            fullName: '', 
+            email: '', 
+            phone: '',
+            id: uniqid()
+        },
+      });
+    };
 
-    return (
-        <div className="create">
-            <h3>Add Personal & Contact Information</h3>
-            <form onSubmit={handleSubmit}>
-                <input 
-                type="text"
-                placeholder="Name"
-                required
-                value={fullName}
-                onChange={(e) => setName(e.target.value)}
-                />
-                <input 
-                type="text"
-                placeholder="Email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                />
-                <input 
-                type="text"
-                placeholder="Phone Number"
-                required
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                />
-                <button className="btn" type="submit">Submit</button>
-                <p>{ fullName }</p>
-                <p>{ email }</p>
-                <p>{ phone }</p>
+    render() {
+        const { info, infoArray } = this.state;
+
+        return (
+          <div>
+            <form onSubmit={this.onSubmitTask}>
+              <input 
+              onChange={this.handleChange}
+              value={info.fullName}
+              placeholder="Name"
+              type="text"
+              name="fullName" />
+              <button type="submit">
+                Add Task
+              </button>
             </form>
-        </div>
-    )
+            <Object infoArray={infoArray} />
+          </div>
+        );
+    }
 }
+
+// const Create = () => {
+//     const [fullName, setName] = useState('');
+//     const [email, setEmail] = useState('');
+//     const [phone, setPhone] = useState('');
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         const inputObj = { fullName, email, phone}
+//         console.log(inputObj);
+//         return inputObj;
+//     }
+
+//     const handleClick = () => {
+//         fetch('')
+//     }
+
+//     return (
+//         <div className="create">
+//             <h3>Add Personal & Contact Information</h3>
+//             <form onSubmit={handleSubmit}>
+//                 <input 
+//                 type="text"
+//                 placeholder="Name"
+//                 required
+//                 value={fullName}
+//                 onChange={(e) => setName(e.target.value)}
+//                 />
+//                 <input 
+//                 type="text"
+//                 placeholder="Email"
+//                 required
+//                 value={email}
+//                 onChange={(e) => setEmail(e.target.value)}
+//                 />
+//                 <input 
+//                 type="text"
+//                 placeholder="Phone Number"
+//                 required
+//                 value={phone}
+//                 onChange={(e) => setPhone(e.target.value)}
+//                 />
+//                 <button className="btn" type="submit">Submit</button>
+//                 <button className="btn" onSubmit={handleClick}>Edit</button>
+//                 <button className="btn" onSubmit={handleClick}>Delete</button>
+//                 <p>{ fullName }</p>
+//                 <p>{ email }</p>
+//                 <p>{ phone }</p>
+//             </form>
+//         </div>
+//     )
+// }
 
 export default Create;
