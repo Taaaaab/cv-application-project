a2a069dbea974646448b508d88bee932a8c91608
Taaaaab <101941764+Taaaaab@users.noreply.github.com>
2022-10-12 11:44:22 -0400

    Update Education.js

--- cv-project/src/components/Education.js
+++ cv-project/src/components/Education.js
@@ -1,70 +1,109 @@
-import { useState } from "react";
+import React, { Component } from "react";
 import "../App.css";
 
-const Education = () => {
-    const [uniName, setUniName] = useState('');
-    const [city, setCity] = useState('');
-    const [degree, setDegree] = useState('');
-    const [subject, setSubject] = useState('');
-    const [from, setFrom] = useState('');
-    const [to, setTo] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const inputObj = { uniName, city, degree, subject, from, to}
-        console.log(inputObj);
+class Education extends Component {
+ constructor(props) {
+     super(props);
+     this.state = {
+         uniName: "",
+         city: "",
+         degree: "",
+         subject: "",
+         from: "",
+         to: "",
+         eduArray: [],
+     };
+     
+     this.handleInputChange = this.handleInputChange.bind(this);
+ }
+    
+    handleInputChange(e) {
+     const target = e.target;
+     const value = target.value;
+     const name = target.name;
+        
+     this.setState({
+        [name]: value, 
+     });
     }
-
-    return (
-        <div className="create">
+    
+    handleSubmit = (e) => {
+     e.preventDefault();
+        const myArray = [
+            `${this.state.uniName}`,
+            `${this.state.city}`,
+            `${this.state.degree}`,
+            `${this.state.subject}`,
+            `${this.state.from}`,
+            `${this.state.to}`,
+            ];
+        console.log(myArray);
+        
+        this.setState({
+            eduArray: this.state.eduArray.concat(myArray),
+            uniName: "",
+            city: "",
+            degree: "",
+            from: "",
+            to: "",
+        });
+    };
+    
+    render() {
+     const { uniName, city, degree, subject, from, to, eduArray } = this.state;
+        
+     return (
+         <div>
             <h3>Add Education Experience</h3>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={this.handleSubmit}>
                 <input 
+                onChange={this.handleInputChange}
+                name="uniName"
                 type="text"
-                placeholder="University Name"
-                required
                 value={uniName}
-                onChange={(e) => setUniName(e.target.value)}
+                placeholder="University Name"
                 />
                 <input 
+                onChange={this.handleInputChange}
+                name="city"
                 type="text"
-                placeholder="City"
-                required
                 value={city}
-                onChange={(e) => setCity(e.target.value)}
+                placeholder="City"
                 />
                 <input 
+                onChange={this.handleInputChange}
+                name="degree"
                 type="text"
-                placeholder="Degree"
-                required
                 value={degree}
-                onChange={(e) => setDegree(e.target.value)}
+                placeholder="Degree"
                 />
                 <input 
+                onChange={this.handleInputChange}
+                name="subject"
                 type="text"
-                placeholder="Subject"
-                required
                 value={subject}
-                onChange={(e) => setSubject(e.target.value)}
+                placeholder="Subject"
                 />
                 <input 
+                onChange={this.handleInputChange}
+                name="from"
                 type="text"
-                placeholder="From"
-                required
                 value={from}
-                onChange={(e) => setFrom(e.target.value)}
+                placeholder="From"
                 />
                 <input 
+                onChange={this.handleInputChange}
+                name="to"
                 type="text"
-                placeholder="To"
-                required
                 value={to}
-                onChange={(e) => setTo(e.target.value)}
+                placeholder="To"
                 />
                 <button className="btn" type="submit">Submit</button>
             </form>
         </div>
-    )
+        );
+    }
+    
 }
-
+   
 export default Education;
