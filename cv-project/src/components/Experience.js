9459c43fd9748d2f6979de2f4f92902cdbbf6292
Taaaaab <owen.whitsett@gmail.com>
2022-10-15 12:16:46 -0400

    Refactor Experience.js to use hooks

--- cv-project/src/components/Experience.js
+++ cv-project/src/components/Experience.js
@@ -1,128 +1,125 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Object from "./Object";
 import "../App.css";
 
-class Experience extends Component {
- constructor(props) {
-     super(props);
-     this.state = {
-         posTitle: "",
-         companyName: "",
-         city: "",
-         tasks: "",
-         from: "",
-         to: "",
-         xpArray: [],
-     };
-     
-     this.handleInputChange = this.handleInputChange.bind(this);
-     this.handleEdit = this.handleEdit.bind(this);
- }
-    
-    handleInputChange(e) {
-     const target = e.target;
-     const value = target.value;
-     const name = target.name;
-        
-     this.setState({
-        [name]: value, 
-     });
-    }
-    
-    handleSubmit = (e) => {
-     e.preventDefault();
-        const myArray = [
-            `${this.state.posTitle}`,
-            `${this.state.companyName}`,
-            `${this.state.city}`,
-            `${this.state.tasks}`,
-            `From: ${this.state.from}`,
-            `To: ${this.state.to}`,
-            ];
-        console.log(myArray);
-        
-        this.setState({
-            xpArray: this.state.xpArray.concat(myArray),
-            posTitle: "",
-            companyName: "",
-            city: "",
-            tasks: "",
-            from: "",
-            to: "",
-        });
+const Experience = () => {
+    let [posTitle, setPosTitle] = useState("");
+    let [companyName, setCompanyName] = useState("");
+    let [city, setCity] = useState("");
+    let [tasks, setTasks] = useState("");
+    let [from, setFrom] = useState("");
+    let [to, setTo] = useState("");
+    let [xpArray, setXpArray] = useState([]);
+
+    const handlePosTitle = (e) => {
+        setPosTitle(posTitle = e.target.value);
     };
- 
-  handleEdit = (e) => {
-        
-     this.setState({
-          xpArray: [],
-          posTitle: "",
-          companyName: "",
-          city: "",
-          tasks: "",
-          from: "",
-          to: "",
-     });
-    }
-    
-    render() {
-     const { posTitle, companyName, city, tasks, from, to, xpArray } = this.state;
-        
-     return (
-         <div>
-            <div className="returnDisplay">
+
+    const handleCompanyName = (e) => {
+        setCompanyName(companyName = e.target.value);
+    };
+
+    const handleCity = (e) => {
+        setCity(city = e.target.value);
+    };
+
+    const handleTasks = (e) => {
+        setTasks(tasks = e.target.value);
+    };
+
+    const handleFrom = (e) => {
+        setFrom(from = e.target.value);
+    };
+
+    const handleTo = (e) => {
+        setTo(to = e.target.value);
+    };
+
+    const handleEdit = (e) => {
+        setPosTitle(posTitle = "");
+        setCompanyName(companyName = "");
+        setCity(city = "");
+        setTasks(tasks = "");
+        setFrom(from = "");
+        setTo(to = "");
+        setXpArray(xpArray = []);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const myArray = [
+            `${posTitle}`,
+            `${companyName}`,
+            `${city}`,
+            `${tasks}`,
+            `From: ${from}`,
+            `To: ${to}`,
+        ];
+        console.log(myArray);
+
+        setXpArray(xpArray = myArray);
+        setPosTitle(posTitle = "");
+        setCompanyName(companyName = "");
+        setCity(city = "");
+        setTasks(tasks = "");
+        setFrom(from = "");
+        setTo(to = "");
+    };
+
+    return (
+        <div>
+<div className="returnDisplay">
             <h3>Work Experience</h3>
               <Object infoArray={xpArray} />
             </div>
             <h3>Add Work Experience</h3>
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handlePosTitle}
                 name="posTitle"
                 type="text"
                 value={posTitle}
                 placeholder="Position Title"
                 />
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleCompanyName}
                 name="companyName"
                 type="text"
                 value={companyName}
                 placeholder="Company Name"
                 />
                 <textarea
-                onChange={this.handleInputChange}
+                onChange={handleTasks}
                 name="tasks"
                 value={tasks}
                 placeholder="Describe tasks and responsibilities"
                 />
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleCity}
                 name="city"
                 type="text"
                 value={city}
                 placeholder="City"
                 />
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleFrom}
                 name="from"
                 type="text"
                 value={from}
                 placeholder="From"
                 />
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleTo}
                 name="to"
                 type="text"
                 value={to}
                 placeholder="To"
                 />
                 <button className="btn" type="submit">Submit</button>
-                <button className="btn" onClick={this.handleEdit}>Edit</button>
+                <button className="btn" onClick={handleEdit}>Edit</button>
             </form>
         </div>
-        );
-    }
+    )
     
 }
    
