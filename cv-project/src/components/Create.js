a93c7d4d868b5b51e9f908bb6afa97a27f827e10
Taaaaab <owen.whitsett@gmail.com>
2022-10-15 10:51:27 -0400

    Replace class with function & hooks

--- cv-project/src/components/Create.js
+++ cv-project/src/components/Create.js
@@ -1,12 +1,12 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState } from "react";
 import Object from "./Object";
 import "../App.css";
 
 const Create = () => {
-  const [fullName, setFullName] = useState(""); 
-  const [email, setEmail] = useState(""); 
-  const [phone, setPhone] = useState("");
-  const [infoArray, setInfoArray] = useState([]);
+  let [fullName, setFullName] = useState(""); 
+  let [email, setEmail] = useState(""); 
+  let [phone, setPhone] = useState("");
+  let [infoArray, setInfoArray] = useState([]);
   
   const handleFullName = (e) => {
     setFullName(fullName = e.target.value);
@@ -38,15 +38,12 @@ const Create = () => {
     
       setInfoArray(infoArray = [myArray]);
       setFullName(fullName = "");
-      setEmail:(email = "");
-      setPhone:(phone = "");
+      (email = "");
+      (phone = "");
   };
   
   return (
      <div>
-      <div>{fullName}</div>
-      <div>{email}</div>
-      <div>{phone}</div>
       <h3>Add Contact Info</h3>
          <form onSubmit={handleSubmit}>
             <input
@@ -56,7 +53,6 @@ const Create = () => {
             value={fullName}
             placeholder="Name"
             />
-//            <button onClick={handleFullName}>Submit Name</button>
             <input
             onChange={handleEmail}
             name="email"
@@ -85,99 +81,3 @@ const Create = () => {
 };
 
 export default Create;
-
-// class Create extends Component {
-//  constructor(props) {
-//    super(props);
-//    this.state = {
-//      fullName: "",
-//      email: "",
-//      phone: "",
-//      infoArray: [],
-//    };
-
-//    this.handleInputChange = this.handleInputChange.bind(this);
-//    this.handleEdit = this.handleEdit.bind(this);
-//  }
-
-//  handleInputChange(e) {
-//    const target = e.target;
-//    const value = target.value;
-//    const name = target.name;
-
-//    this.setState({
-//      [name]: value,
-//    });
-//  }
-//  handleSubmit = (e) => {
-//    e.preventDefault();
-//    const myArray = [
-//      `${this.state.fullName}`,
-//      `${this.state.email}`,
-//      `${this.state.phone}`,
-//    ];
-//    console.log(myArray);
-
-//    this.setState({
-//      infoArray: this.state.infoArray.concat(myArray),
-//      fullName: "",
-//      email: "",
-//      phone: "",
-
-//    });
-//  };
-  
-//    handleEdit = (e) => {
-   
-//    this.setState({
-//      infoArray: [],
-//      fullName: "",
-//      email: "",
-//      phone: "",
-
-//    });
-//  };
-
-//  render() {
-//    const { fullName, email, phone, infoArray } = this.state;
-
-//    return (
-//      <div>
-//        <h3>Add Contact Info</h3>
-//        <form onSubmit={this.handleSubmit}>
-//          <input
-//            onChange={this.handleInputChange}
-//            name="fullName"
-//            type="text"
-//            value={fullName}
-//            placeholder="Name"
-//          />
-//          <input
-//            onChange={this.handleInputChange}
-//            name="email"
-//            type="text"
-//            value={email}
-//            placeholder="Email"
-//          />
-//          <input
-//            onChange={this.handleInputChange}
-//            name="phone"
-//            type="text"
-//            value={phone}
-//            placeholder="Phone"
-//          />
-//          <button type="submit" className="btn">Submit</button>
-//          <button onClick={this.handleEdit} className="btn">Edit</button>
-//        </form>
-//        <div className="returnDisplay">
-//          <h3 class="title">Contact Information</h3>
-//          <div className="returnArray">
-//          <Object infoArray={infoArray} />
-//          </div>
-//        </div>
-//      </div>
-//    );
-//  }
-// }
-
-// export default Create;
