553053d95a864a58d4302a4fce729c270c244065
Taaaaab <owen.whitsett@gmail.com>
2022-10-15 12:28:54 -0400

    Refactor Education.js using hooks

--- cv-project/src/components/Education.js
+++ cv-project/src/components/Education.js
@@ -1,130 +1,126 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Object from "./Object";
 import "../App.css";
 
-class Education extends Component {
- constructor(props) {
-     super(props);
-     this.state = {
-         uniName: "",
-         city: "",
-         degree: "",
-         subject: "",
-         from: "",
-         to: "",
-         eduArray: [],
-     };
-     
-     this.handleInputChange = this.handleInputChange.bind(this);
-     this.handleEdit = this.handleEdit.bind(this);
- }
-    
-    handleInputChange(e) {
-     const target = e.target;
-     const value = target.value;
-     const name = target.name;
-        
-     this.setState({
-        [name]: value, 
-     });
-    }
-    
-    handleSubmit = (e) => {
-     e.preventDefault();
-        const myArray = [
-            `${this.state.uniName}`,
-            `${this.state.city}`,
-            `${this.state.degree}`,
-            `${this.state.subject}`,
-            `From: ${this.state.from}`,
-            `To: ${this.state.to}`,
-            ];
-        console.log(myArray);
-        
-        this.setState({
-            eduArray: this.state.eduArray.concat(myArray),
-            uniName: "",
-            city: "",
-            degree: "",
-            from: "",
-            to: "",
-        });
+const Education = () => {
+    let [uniName, setUniName] = useState("");
+    let [city, setCity] = useState("");
+    let [degree, setDegree] = useState("");
+    let [subject, setSubject] = useState("");
+    let [from, setFrom] = useState("");
+    let [to, setTo] = useState("");
+    let [eduArray, setEduArray] = useState([]);
+
+    const handleUniName = (e) => {
+        setUniName(uniName = e.target.value);
     };
 
-    handleEdit = (e) => {
-        
-        this.setState({
-            uniName: "",
-            city: "",
-            degree: "",
-            subject: "",
-            from: "",
-            to: "",
-            eduArray: [],
-        });
-       }
-     
-    
-    render() {
-     const { uniName, city, degree, subject, from, to, eduArray } = this.state;
-        
-     return (
-         <div>
+    const handleCity = (e) => {
+        setCity(city = e.target.value);
+    };
+
+    const handleDegree = (e) => {
+        setDegree(degree = e.target.value);
+    };
+
+    const handleSubject = (e) => {
+        setSubject(subject = e.target.value);
+    };
+
+    const handleFrom = (e) => {
+        setFrom(from = e.target.value);
+    };
+
+    const handleTo = (e) => {
+        setTo(to = e.target.value);
+    };
+
+    const handleEdit = (e) => {
+        setUniName(uniName = "");
+        setCity(city = "");
+        setDegree(degree = "");
+        setSubject(subject = "");
+        setFrom(from = "");
+        setTo(to = "");
+        setEduArray(eduArray = []);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const myArray = [
+            `${uniName}`,
+            `${city}`,
+            `${degree}`,
+            `${subject}`,
+            `From: ${from}`,
+            `To: ${to}`,
+        ];
+        console.log(myArray);
+
+        setEduArray(eduArray = myArray);
+        setUniName(uniName = "");
+        setCity(city = "");
+        setDegree(degree = "");
+        setSubject(subject = "");
+        setFrom(from = "");
+        setTo(to = "");
+    };
+
+    return (
+<div>
             <div className="returnDisplay">
               <h3 className="title">Education</h3>
               <Object infoArray={eduArray} />
             </div>
             <h3>Add Education Experience</h3>
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleUniName}
                 name="uniName"
                 type="text"
                 value={uniName}
                 placeholder="University Name"
                 />
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleCity}
                 name="city"
                 type="text"
                 value={city}
                 placeholder="City"
                 />
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleDegree}
                 name="degree"
                 type="text"
                 value={degree}
                 placeholder="Degree"
                 />
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleSubject}
                 name="subject"
                 type="text"
                 value={subject}
                 placeholder="Subject"
                 />
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleFrom}
                 name="from"
                 type="text"
                 value={from}
                 placeholder="From"
                 />
                 <input 
-                onChange={this.handleInputChange}
+                onChange={handleTo}
                 name="to"
                 type="text"
                 value={to}
                 placeholder="To"
                 />
                 <button className="btn" type="submit">Submit</button>
-                <button className="btn" onClick={this.handleEdit}>Edit</button>
+                <button className="btn" onClick={handleEdit}>Edit</button>
             </form>
         </div>
-        );
-    }
-    
+    )
 }
-   
+        
 export default Education;
